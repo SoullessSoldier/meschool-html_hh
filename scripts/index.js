fedc6ce6542365adb6d3a5a3f2222a984419a7ab
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2022-03-11 22:57:02 +0200

    add Day 3

--- scripts/index.js
+++ scripts/index.js
@@ -7,96 +7,36 @@ const optionBtnOrder = document.querySelector('.option__btn_order'),
     optionListPeriod = document.querySelector('.option__list_period'),
     headerFound = document.querySelector('.found');
 
-optionBtnOrder.addEventListener('click', (e) => {
-    optionListOrder.classList.toggle('option__list_active');
-    optionListPeriod.classList.remove('option__list_active');
-});
-
-optionBtnPeriod.addEventListener('click', (e) => {
-    optionListPeriod.classList.toggle('option__list_active');
-    optionListOrder.classList.remove('option__list_active');
-});
-
-optionListOrder.addEventListener('click', (e) => {
-    const target = e.target;
-    if (target.classList.contains('option__item')){
-        optionBtnOrder.textContent = target.textContent;
-        for (const elem of optionListOrder.querySelectorAll('.option__item')){
-            if (elem === target){
-                elem.classList.add('option__item_active');
-            } else {
-                elem.classList.remove('option__item_active');
-            }
-        }        
-    }
-    optionListOrder.classList.remove('option__list_active');
-});
-
-optionListPeriod.addEventListener('click', (e) => {
-    const target = e.target;
-    if (target.classList.contains('option__item')){
-        optionBtnPeriod.textContent = target.textContent;
-        for (const elem of optionListPeriod.querySelectorAll('.option__item')){
-            if (elem === target){
-                elem.classList.add('option__item_active');
-            } else {
-                elem.classList.remove('option__item_active');
-            }
-        }        
-    }
-    optionListPeriod.classList.remove('option__list_active');
-});
-
-//выбор города
 const topCityBtn = document.querySelector('.top__city'),
     city = document.querySelector('.city'),
     cityCloseBtn = document.querySelector('.city__close'),
     cityRegionList = document.querySelector('.city__region-list');
 
-topCityBtn.addEventListener('click', () => {
-    city.classList.toggle('city_active');
-});
-
-cityCloseBtn.addEventListener('click', () => {
-    city.classList.remove('city_active');
-});
-
-cityRegionList.addEventListener('click', (e) => {
-    const target = e.target;
-    if(target.classList.contains('city__link')){
-        topCityBtn.textContent = target.textContent;
-        city.classList.remove('city_active');
-    }
-});
-
-//модальное окно
 const overlayVacancyModal = document.querySelector('.overlay_vacancy'),
     resultList = document.querySelector('.result__list');
 
-resultList.addEventListener('click', (e) => {    
-    const target = e.target;
-    if(target.dataset.vacancy){
-        e.preventDefault();
-        overlayVacancyModal.classList.add('overlay_active');
-    }
-});
+const formSearch = document.querySelector('.bottom__search');
 
-overlayVacancyModal.addEventListener('click', (e) => {
-    const target = e.target;
-    if (target === overlayVacancyModal || target.classList.contains('modal__close')){
-        overlayVacancyModal.classList.remove('overlay_active');
-    }
-});
+const BASE_URL = 'http://localhost:3000';
+const urlApiGetVacancy = new URL('api/vacancy', BASE_URL);
 
-//вывод карточек и поиск
+const createUrlApiGetVacancyById = (id = '') => {
+    let res ;
+    if (id) {
+        res = new URL(`api/vacancy/${id}`, BASE_URL);
+    } else {
+        res = urlApiGetVacancy;
+    }
+    return res;
+};
+
 const convertDate = (dateString = '') => {    
     const patternDate  = /(\d{2})\/(\d{2})\/(\d{4})/gm;
     let withGyhepns, withDots;
     withGyhepns = dateString ? dateString.replace(patternDate, '$3-$1-$2') : '';
     withDots  = dateString ? dateString.replace(patternDate, '$2.$1.$3') : '';
-    console.log(dateString, withGyhepns, withDots);
     return {withGyhepns, withDots};    
-}
+};
 
 const createCard = (vacancy) => {
     const {id,
@@ -149,53 +89,190 @@ const renderCards = (data = ['']) => {
 
 };
 
-const BASE_URL = 'http://localhost:3000';
-const urlApiGetVacancy = new URL('api/vacancy', BASE_URL);
 
-const getData = ({search} = {}) => {
+
+const getData = ({search, id} = {}) => {
     if (search) {
         urlApiGetVacancy.searchParams.delete('search');
         urlApiGetVacancy.searchParams.append('search', search);
         return fetch(urlApiGetVacancy);
     }
-    return fetch(urlApiGetVacancy);
-}
-
-const formSearch = document.querySelector('.bottom__search');
-
-formSearch.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const textSearch = formSearch.search.value;
-    if (textSearch.length > 2) {
-        formSearch.search.style.borderColor = '';
-        const data = await getData({search: textSearch})
-        .then(response => {
-            if(response.ok){
-                return response.json();
-            } 
-            throw new Error('Something went wrong');
-        })
-        .then(data => {
-            return data;
-        })    
-        .catch((error) => {
-            console.warn(error);
-        });
-        renderCards(data);
-        formSearch.reset();
-        clearHeaderFound();
-        const headerFoundContent = `
-        ${data.length} ${declOfNum(data.length, ['вакансия', 'вакансии', 'вакансий'])} &laquo;<span class="found__item">${textSearch}</span>&raquo;
-        `;
-        headerFound.insertAdjacentHTML('beforeend', headerFoundContent);
-
+    if (id) {
+        const url = createUrlApiGetVacancyById(id);
+        console.log(url);    
+        return fetch(url);
     } else {
-        formSearch.search.style.borderColor = 'red';
-        setTimeout(()=>{
-            formSearch.search.style.borderColor = '';
-        }, 2000);
+        return fetch(urlApiGetVacancy);
     }
-});
+    
+};
+
+const handlerOptions = () => {
+    optionBtnOrder.addEventListener('click', (e) => {
+        optionListOrder.classList.toggle('option__list_active');
+        optionListPeriod.classList.remove('option__list_active');
+    });
+    
+    optionBtnPeriod.addEventListener('click', (e) => {
+        optionListPeriod.classList.toggle('option__list_active');
+        optionListOrder.classList.remove('option__list_active');
+    });
+    
+    optionListOrder.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.classList.contains('option__item')){
+            optionBtnOrder.textContent = target.textContent;
+            for (const elem of optionListOrder.querySelectorAll('.option__item')){
+                if (elem === target){
+                    elem.classList.add('option__item_active');
+                } else {
+                    elem.classList.remove('option__item_active');
+                }
+            }        
+        }
+        optionListOrder.classList.remove('option__list_active');
+    });
+    
+    optionListPeriod.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.classList.contains('option__item')){
+            optionBtnPeriod.textContent = target.textContent;
+            for (const elem of optionListPeriod.querySelectorAll('.option__item')){
+                if (elem === target){
+                    elem.classList.add('option__item_active');
+                } else {
+                    elem.classList.remove('option__item_active');
+                }
+            }        
+        }
+        optionListPeriod.classList.remove('option__list_active');
+    });
+};
+
+const handlerCity = () => {
+    topCityBtn.addEventListener('click', () => {
+        city.classList.toggle('city_active');
+    });
+    
+    cityCloseBtn.addEventListener('click', () => {
+        city.classList.remove('city_active');
+    });
+    
+    cityRegionList.addEventListener('click', (e) => {
+        const target = e.target;
+        if(target.classList.contains('city__link')){
+            topCityBtn.textContent = target.textContent;
+            city.classList.remove('city_active');
+        }
+    });
+};
+
+const createModal = (data) => {
+    const {        
+        title,
+        compensation,
+        employer,
+        address,
+        experience,
+        description,
+        employment,
+        skills
+    } = data;
+    const modal = document.createElement('div');
+    let skillsList = '';
+    modal.classList.add('modal');
+    for (let item of skills) {
+        skillsList += `<li class="skills__item">${item}</li>\n`;
+    }
+    const modalContent = `
+    <button class="modal__close">✕</button>
+    <h3 class="modal__title">${title}</h3>
+    <p class="modal__compensation">${compensation}</p>
+    <p class="modal__employer">${employer}</p>
+    <p class="modal__address">${address}</p>
+    <p class="modal__experience">Требуемый опыт работы: ${experience}</p>
+    <p class="modal__employment">${employment.join(',')}</p>
+    <p class="modal__description">${description}</p>
+    <div class="modal__skills skills">
+      <h2 class="skills__title">Подробнее:</h2>
+      <ul class="skills__list">${skillsList}         
+      </ul>
+    </div>
+
+    <button class="modal__response">Отправить резюме</button>
+    `;
+    modal.insertAdjacentHTML('beforeend', modalContent);
+    return modal;
+};
+
+const handlerModal = () => {
+    resultList.addEventListener('click', async (e) => {    
+        const target = e.target;
+        if(target.dataset.vacancy){
+            e.preventDefault();
+            overlayVacancyModal.classList.add('overlay_active');
+            overlayVacancyModal.textContent = '';
+            const data = await getData({id: target.dataset.vacancy})
+            .then(response => {
+                if(response.ok){
+                    return response.json();
+                } 
+                throw new Error('Something went wrong');
+            })
+            .then(data => {
+                return data;
+            })    
+            .catch((error) => {
+                console.warn(error);
+            });
+            const modal = createModal(data);
+            overlayVacancyModal.append(modal);
+        }
+    });
+    
+    overlayVacancyModal.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target === overlayVacancyModal || target.classList.contains('modal__close')){
+            overlayVacancyModal.classList.remove('overlay_active');
+        }
+    });
+};
+
+const handlerSearch = () => {
+    formSearch.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const textSearch = formSearch.search.value;
+        if (textSearch.length > 2) {
+            formSearch.search.style.borderColor = '';
+            const data = await getData({search: textSearch})
+            .then(response => {
+                if(response.ok){
+                    return response.json();
+                } 
+                throw new Error('Something went wrong');
+            })
+            .then(data => {
+                return data;
+            })    
+            .catch((error) => {
+                console.warn(error);
+            });
+            renderCards(data);
+            formSearch.reset();
+            clearHeaderFound();
+            const headerFoundContent = `
+            ${data.length} ${declOfNum(data.length, ['вакансия', 'вакансии', 'вакансий'])} &laquo;<span class="found__item">${textSearch}</span>&raquo;
+            `;
+            headerFound.insertAdjacentHTML('beforeend', headerFoundContent);
+    
+        } else {
+            formSearch.search.style.borderColor = 'red';
+            setTimeout(()=>{
+                formSearch.search.style.borderColor = '';
+            }, 2000);
+        }
+    });
+};
 
 const init = async () => {
     const data = await getData()
@@ -211,19 +288,27 @@ const init = async () => {
     .catch((error) => {
         console.warn(error);
     });
-    renderCards(data);        
+    renderCards(data);
+    
+    handlerOptions();
+    
+    handlerCity();
+    
+    handlerModal();    
+    
+    handlerSearch();
         
 };
 
-const clearHeaderFound = () => {
-    headerFound.textContent = '';
+const clearHeaderFound = (word = '') => {
+    headerFound.textContent = word;
 };
 
-function declOfNum(number, words) {  
+const declOfNum = (number, words) => {  
     return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
-} 
+}; 
 
-clearHeaderFound();
+clearHeaderFound('Последние вакансии');
 init();
 
 renderCards();
